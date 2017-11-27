b7e899d8a26b2664d5b6317c79a02bcd86c69e05
nik <fullcrimp@protonmail.com>
2017-11-27 11:53:20 +0300

    add brute force solution; correct tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
+  let result = []
+  for(let i = 0, key, val; i < boolset.length; i += 2) {
+      key = boolset[i]
+      val = boolset[i + 1]
+      if(val) {
+          result.push(key)
+      }
+  }
+  return result
 }
